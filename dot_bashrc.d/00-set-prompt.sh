c8ff34cbe10febc8112c78012f992adc2735d732
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-10-02 02:08:41 +0200

    Bash configuration

---
+++ dot_bashrc.d/00-set-prompt.sh
@@ -0,0 +1,19 @@
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\n\$ '
+fi
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
---
+++ dot_bashrc.d/enable-kitty-integration.sh
@@ -0,0 +1,3 @@
+# BEGIN_KITTY_SHELL_INTEGRATION
+if test -e "$HOME/.local/kitty.app/lib/kitty/shell-integration/kitty.bash"; then source "$HOME/.local/kitty.app/lib/kitty/shell-integration/kitty.bash"; fi
+# END_KITTY_SHELL_INTEGRATION
