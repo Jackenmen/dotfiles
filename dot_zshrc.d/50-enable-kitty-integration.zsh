f871d5bf73a967b59d68e7a4ac481dcbdb56e7ae
jack1142 <6032823+jack1142@users.noreply.github.com>
2022-01-07 01:09:05 +0100

    Update kitty integration to work with kitty 0.24

--- dot_zshrc.d/50-enable-kitty-integration.zsh
+++ dot_zshrc.d/50-enable-kitty-integration.zsh
@@ -1,3 +1,3 @@
 # BEGIN_KITTY_SHELL_INTEGRATION
-if test -e "$HOME/.local/kitty.app/lib/kitty/shell-integration/kitty.zsh"; then source "$HOME/.local/kitty.app/lib/kitty/shell-integration/kitty.zsh"; fi
+if test -n "$KITTY_INSTALLATION_DIR" -a -e "$KITTY_INSTALLATION_DIR/shell-integration/zsh/kitty.zsh"; then source "$KITTY_INSTALLATION_DIR/shell-integration/zsh/kitty.zsh"; fi
 # END_KITTY_SHELL_INTEGRATION
