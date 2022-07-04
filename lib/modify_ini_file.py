0e6f6a25080adfc0b41b51c3cd8fb6165eee2a74
jack1142 <6032823+jack1142@users.noreply.github.com>
2022-07-04 17:00:37 +0200

    Fix INI file modification to add missing sections and keys

--- lib/modify_ini_file.py
+++ lib/modify_ini_file.py
@@ -23,32 +23,44 @@ import sys
 SECTIONS_TO_MODIFY = json.loads(sys.argv[1])
 
 section_name_pattern = "|".join(re.escape(name) for name in SECTIONS_TO_MODIFY)
-SECTION_PATTERN_RE = re.compile(rf"\[({section_name_pattern})\]\n[\s\S]*?\n(\n|$)")
+SECTION_PATTERN_RE = re.compile(rf"\[({section_name_pattern})\]\n[\s\S]*?\n(?=\n|$)")
 
 original_contents = sys.stdin.read()
+FOUND_SECTIONS = set()
+
 
 def repl(match: re.Match[str]) -> str:
+    FOUND_SECTIONS.add(match.group(1))
+    found_keys = set()
     to_add = SECTIONS_TO_MODIFY[match.group(1)]
     lines = match.group(0).splitlines(keepends=True)
     for idx, line in enumerate(lines):
         key, _, value = line.partition("=")
         if not value:
             continue
+        found_keys.add(key)
         new_value = to_add.get(key)
         if new_value is not None:
             lines[idx] = f"{key}={to_add[key]}\n"
+
+    for key, value in to_add.items():
+        if key not in found_keys:
+            lines.append(f"{key}={value}\n")
 
     return f"{''.join(lines)}"
 
 
 if original_contents:
-    print(SECTION_PATTERN_RE.sub(repl, original_contents), end="")
+    lines = SECTION_PATTERN_RE.sub(repl, original_contents).splitlines(keepends=True)
 else:
     lines = []
-    for section_name, to_add in SECTIONS_TO_MODIFY.items():
-        lines.append(f"[{section_name}]\n")
-        for key, value in to_add.items():
-            lines.append(f"{key}={value}\n")
-        lines.append("\n")
-    lines.pop()
-    print("".join(lines), end="")
+
+for section_name, to_add in SECTIONS_TO_MODIFY.items():
+    if section_name in FOUND_SECTIONS:
+        continue
+    lines.append(f"[{section_name}]\n")
+    for key, value in to_add.items():
+        lines.append(f"{key}={value}\n")
+    lines.append("\n")
+lines.pop()
+print("".join(lines), end="")
